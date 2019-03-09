7d513359ba680713ab7d4550be0bfe35e5e07cc0
Stephen Frost <sfrost@snowman.net>
2019-03-08 19:00:39 -0500

    updates

--- code/pgusinvoices.py
+++ code/pgusinvoices.py
@@ -5,29 +5,19 @@ from postgresqleu.util.misc.baseinvoice import BaseInvoice,BaseRefund
 
 class PGUSBase(object):
     logo = os.path.join(settings.PROJECT_ROOT, '../media/img/PostgreSQL_logo.1color_blue.300x300.png')
-    headertext = """PostgreSQL Europe
-Carpeaux Diem
-13, rue du Square Carpeaux
-75018 PARIS
-France
-SIREN 823839535
-VAT# FR36823839535
+    headertext = """United States PostgreSQL Association
+9220 SW Barbur Blvd
+Ste 119-230
+Portland, OR, 97219
 """
-    sendertext = """Your contact: Guillaume Lelarge
-Function: PostgreSQL Europe Treasurer
-E-mail: treasurer@postgresql.eu"""
+    sendertext = """Your contact: Mark Wong
+Function: United States PostgreSQL Association Treasurer
+E-mail: treasurer@postgresql.us"""
 
-    bankinfotext = """CCM PARIS 1-2 LOUVRE MONTORGUEIL
-28 RUE ETIENNE MARCEL
-75002 PARIS
-FRANCE
-IBAN: FR76 1027 8060 3100 0205 2290 114
-BIC: CMCIFR2A
+    bankinfotext = """Contact treasurer@postgresql.us
 """
 
-    paymentterms = """Penalty for late payment: Three times the French Legal Interest Rate on the due amount.
-Compensation due for any recovery costs incurred: â‚¬40
-Discount for prepayment: None.
+    paymentterms = """
 """
 
 
--- code/skin_local_settings.py
+++
@@ -1,9 +0,0 @@
-SERVER_EMAIL="sfrost@snowman.net"
-
-INVOICE_SENDER_EMAIL="sfrost@snowman.net"
-MEMBERSHIP_SENDER_EMAIL="sfrost@snowman.net"
-
-TREASURER_EMAIL="sfrost@snowman.net"
-
-# Base URLs for generating absolute URLs
-SITEBASE="https://test.postgresql.us"

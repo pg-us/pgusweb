52adf2482746c445ffb642dfa9f55561b1d96ddf
Kevin Rocker <me@kevinrocker.com>
2026-03-11 19:30:31 +0100

    Update Diversity to Community Development

--- code/skin_urls.py
+++ code/skin_urls.py
@@ -1,1 +1,6 @@
-PRELOAD_URLS= [ ]
+from django.urls import re_path
+from django.views.generic import RedirectView
+
+PRELOAD_URLS = [
+    re_path(r'^diversity/?$', RedirectView.as_view(url='/community_development/', permanent=True)),
+]

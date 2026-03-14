6731f4506d72ec3e6f54caa635f62af934e21f84
Kevin Rocker <me@kevinrocker.com>
2026-03-14 18:20:05 +0100

    Update Diversity Title, Redirect

--- code/skin_urls.py
+++ code/skin_urls.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from django.views.generic import RedirectView
 
 PRELOAD_URLS = [
-    re_path(r'^diversity/?$', RedirectView.as_view(url='/community_development/', permanent=True)),
+    re_path(r'^diversity/$', RedirectView.as_view(url='/community_development/', permanent=True)),
 ]
